33eed8ba9aa3c7364b15670b3890d1be891a7ce8
Eli Moshkovich <tbhjfo14@gmail.com>
2017-03-29 15:51:36 +0300

    add boolean field only_one

--- BranchA_MazalPlus/BranchA_MazalPlus/Secretary.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Secretary.cs
@@ -8,11 +8,17 @@ namespace BranchA_MazalPlus
 {
     class Secretary:Person
     {
+        private static Boolean only_one=false;
         public Secretary() : base() {; }
 
-        public Secretary(string ID, string FIRname, string LAname, string TELE, string EMA, string PASS) : base(ID, FIRname, LAname, TELE, EMA, PASS, "Secretary") {; }
-       
-
+        public Secretary(string ID, string FIRname, string LAname, string TELE, string EMA, string PASS) : base(ID, FIRname, LAname, TELE, EMA, PASS, "Secretary")
+        {
+            if (only_one == false)
+            {
+                only_one = true;
+            }
+            else System.Windows.Forms.MessageBox.Show("There is only one Secretary possible");
+        }
 
 
     }
