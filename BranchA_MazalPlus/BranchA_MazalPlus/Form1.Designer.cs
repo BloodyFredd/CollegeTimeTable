bb61fa4f6cd4f660919ba9dedc34b8e633585c62
Daniel Bromberg <Daniebr1@ac.sce.ac.il>
2017-03-29 12:46:46 +0300

    Revert "Login"
    This reverts commit 944f20535242a15552ee7b4072fe8a0e6518084e.

--- BranchA_MazalPlus/BranchA_MazalPlus/Form1.Designer.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Form1.Designer.cs
@@ -28,17 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1082, 796);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
