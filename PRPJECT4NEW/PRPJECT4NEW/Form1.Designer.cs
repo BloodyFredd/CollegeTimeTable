bf82536fcbb472ec4a97b1600ba1f8599f0880d9
Daniel Bromberg <Daniebr1@ac.sce.ac.il>
2017-03-29 11:48:54 +0300

    color change

--- PRPJECT4NEW/PRPJECT4NEW/Form1.Designer.cs
+++ PRPJECT4NEW/PRPJECT4NEW/Form1.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(844, 469);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 379);
+            this.ClientSize = new System.Drawing.Size(1200, 734);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
