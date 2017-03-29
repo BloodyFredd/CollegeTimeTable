3a8b5ebd7ce3af92afa3e24ebd49d0a088853482
adir abhuazera <adirab@ac.sce.ac.il>
2017-03-29 13:19:21 +0300

    PERSON

--- BranchA_MazalPlus/BranchA_MazalPlus/Person.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Person.cs
@@ -8,5 +8,27 @@ namespace BranchA_MazalPlus
 {
     class Person
     {
+        private int id;
+        private string F_name;
+        private string L_name;
+        private string Telephone;
+        private string Email;
+        private string Password;
+        private string Permission;
+
+        public Person()
+        {
+            
+        }
+        public Person(int ID,string FIRname,string LAname,string TELE,string EMA,string PASS,string PERM)
+        {
+            id = ID;
+            F_name = FIRname;
+            L_name = LAname;
+            Telephone = TELE;
+            Email = EMA;
+            Password = PASS;
+            Permission = PERM;
+        }
     }
 }
