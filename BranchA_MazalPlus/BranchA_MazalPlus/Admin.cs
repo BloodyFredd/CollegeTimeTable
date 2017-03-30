cb1634cbb9527819874a3cabb26fb3891bb17e7a
Freddy M <fredtein@gmail.com>
2017-03-30 17:41:53 +0300

    Admin

--- BranchA_MazalPlus/BranchA_MazalPlus/Admin.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Admin.cs
@@ -7,18 +7,23 @@ using System.Threading.Tasks;
 namespace BranchA_MazalPlus
 {
     class Admin : Person
+    {
+        private static bool only_one = false;
+        public Admin() : base() {; }
+
+        public Admin(string ID, string FIRname, string LAname, string TELE, string EMA, string PASS) : base(ID, FIRname, LAname, TELE, EMA, PASS, "Secretary")
         {
-            private static bool only_one = false;
-            public Admin() : base() {; }
-
-            public Admin(string ID, string FIRname, string LAname, string TELE, string EMA, string PASS) : base(ID, FIRname, LAname, TELE, EMA, PASS, "Secretary")
+            if (only_one == false)
             {
-                if (only_one == false)
-                {
-                    only_one = true;
-                }
-                else System.Windows.Forms.MessageBox.Show("ERROR:\nThere is only one Admin possible\n");
+                only_one = true;
             }
-
+            else System.Windows.Forms.MessageBox.Show("ERROR:\nThere is only one Admin possible\n");
         }
+        public void makeUser()
+        {
+            System.Windows.Forms.MessageBox.Show("Hello " + this.F_name + "! Please enter which user you would like to add:\n");
+            // Add here a box for adding a new person.
+        }
+
+    }
 }
