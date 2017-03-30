58e1720152cfa2e4cedde9c50d75b4ef939642db
adir abhuazera <adirab@ac.sce.ac.il>
2017-03-30 19:34:01 +0300

    but

--- BranchA_MazalPlus/BranchA_MazalPlus/Admin.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Admin.cs
@@ -24,6 +24,7 @@ namespace BranchA_MazalPlus
             System.Windows.Forms.MessageBox.Show("Hello " + this.F_name + "! Please enter which user you would like to add:\n");
             //System.Windows.Forms.MessageBox.Show();
             // Add here a box for adding a new person.
+            //gchv
         }
 
     }
