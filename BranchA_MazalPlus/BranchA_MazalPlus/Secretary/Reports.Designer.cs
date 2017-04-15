23366d4a0b2908039f2d7fd2b87fb76c624359f7
adir abhuazera <adirab@ac.sce.ac.il>
2017-04-15 18:17:54 +0300

    ADIR THE ADIR

--- BranchA_MazalPlus/BranchA_MazalPlus/Secretary/Reports.Designer.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Secretary/Reports.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // Load_table
             // 
-            this.Load_table.Location = new System.Drawing.Point(29, 401);
+            this.Load_table.Location = new System.Drawing.Point(22, 326);
+            this.Load_table.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Load_table.Name = "Load_table";
-            this.Load_table.Size = new System.Drawing.Size(198, 45);
+            this.Load_table.Size = new System.Drawing.Size(148, 37);
             this.Load_table.TabIndex = 3;
             this.Load_table.Text = "Show Students only!";
             this.Load_table.UseVisualStyleBackColor = true;
@@ -47,18 +48,20 @@
             // StudentsReport
             // 
             this.StudentsReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.StudentsReport.Location = new System.Drawing.Point(29, 12);
+            this.StudentsReport.Location = new System.Drawing.Point(22, 10);
+            this.StudentsReport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.StudentsReport.Name = "StudentsReport";
             this.StudentsReport.RowTemplate.Height = 24;
-            this.StudentsReport.Size = new System.Drawing.Size(659, 366);
+            this.StudentsReport.Size = new System.Drawing.Size(494, 297);
             this.StudentsReport.TabIndex = 2;
             this.StudentsReport.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentsReport_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(268, 401);
+            this.button1.Location = new System.Drawing.Point(201, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(246, 45);
+            this.button1.Size = new System.Drawing.Size(184, 37);
             this.button1.TabIndex = 4;
             this.button1.Text = "Show Teaching Staff only!";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,15 +69,15 @@
             // 
             // Reports
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 473);
+            this.ClientSize = new System.Drawing.Size(533, 384);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Load_table);
             this.Controls.Add(this.StudentsReport);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Reports";
             this.Text = "Reports";
-            //this.Load += new System.EventHandler(this.Reports_Load);
             ((System.ComponentModel.ISupportInitialize)(this.StudentsReport)).EndInit();
             this.ResumeLayout(false);
 
