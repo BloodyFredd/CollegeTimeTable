1aaf8e226a11c6940e72ea86f97a871f277a4b1c
adir abhuazera <adirab@ac.sce.ac.il>
2017-05-04 15:51:22 +0300

    student assghied

--- BranchA_MazalPlus/BranchA_MazalPlus/Lecturer/Reports.Designer.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Lecturer/Reports.Designer.cs
@@ -38,6 +38,7 @@
             this.bToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
             this.LecturerCourses = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.StudentsReport)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -300,20 +301,31 @@
             // 
             // LecturerCourses
             // 
-            this.LecturerCourses.Location = new System.Drawing.Point(423, 430);
-            this.LecturerCourses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LecturerCourses.Location = new System.Drawing.Point(380, 430);
+            this.LecturerCourses.Margin = new System.Windows.Forms.Padding(2);
             this.LecturerCourses.Name = "LecturerCourses";
-            this.LecturerCourses.Size = new System.Drawing.Size(132, 45);
+            this.LecturerCourses.Size = new System.Drawing.Size(166, 45);
             this.LecturerCourses.TabIndex = 10;
             this.LecturerCourses.Text = "Lecturer_Courses";
             this.LecturerCourses.UseVisualStyleBackColor = true;
             this.LecturerCourses.Click += new System.EventHandler(this.LecturerCourses_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 45);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Students";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Reports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(874, 519);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.LecturerCourses);
             this.Controls.Add(this.Available_Classes);
             this.Controls.Add(this.menuStrip1);
@@ -342,5 +354,6 @@
         private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
         private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
         private System.Windows.Forms.Button LecturerCourses;
+        private System.Windows.Forms.Button button2;
     }
 }
