977da4c2eef1253ce8429dcf72305c895c747962
Freddy M <fredtein@gmail.com>
2017-05-04 16:33:02 +0300

    Teaching_Assistant:  Alongside lecturers-not done

--- BranchA_MazalPlus/BranchA_MazalPlus/Teaching_Assistant/Reports.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Teaching_Assistant/Reports.cs
@@ -32,14 +32,18 @@ namespace BranchA_MazalPlus.Teaching_Assistant
 
         private void Load_table_Click_1(object sender, EventArgs e)
         {
-
             this.connetionString = "Data Source = whitesnow.database.windows.net; Initial Catalog = Mazal; Integrated Security = False; User ID = Grimm; Password = #!7Dwarfs; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
             this.sqlcon = new SqlConnection(connetionString);
-            SqlCommand cmd = new SqlCommand("select person.ID,person.F_name,person.Lname,Student_Courses.final_grade from person join Student_Courses on Person.ID=Student_Courses.stud_id where Student_Courses.final_grade <56"+"'", sqlcon);
 
+            //SqlCommand cmd = new SqlCommand("select distinct t.Course_id from Teaching_Stuff t where not exists(select ID, Course_id from Teaching_Stuff t2 where t2.Course_id = t.Course_id)", sqlcon);
+            SqlCommand cmd = new SqlCommand("SELECT DISTINCT t.Course_id, t.ID FROM Teaching_Stuff t WHERE EXISTS( SELECT t2.Course_id FROM Teaching_Stuff t2 WHERE t2.ID != '" + Forms.UserID.ID + "')", sqlcon);
+
+            //this.sqlcon.Open();
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //SqlCommand cmd1 = new SqlCommand("select ID from Teaching_Stuff where Course_id ='" + dr[0].ToString() + "'", sqlcon);
             try
             {
- 
+
                 SqlDataAdapter sda = new SqlDataAdapter();
                 sda.SelectCommand = cmd;
                 DataTable dbdataset = new DataTable();
@@ -61,14 +65,15 @@ namespace BranchA_MazalPlus.Teaching_Assistant
             this.connetionString = "Data Source = whitesnow.database.windows.net; Initial Catalog = Mazal; Integrated Security = False; User ID = Grimm; Password = #!7Dwarfs; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
             this.sqlcon = new SqlConnection(connetionString);
 
-            SqlCommand cmd = new SqlCommand("select ID,F_name,L_name,Telephone,Email from person where Permission='Teaching_Assistant' OR Permission='Lecturer' ", sqlcon);
-
-
+            SqlCommand cmd = new SqlCommand("select Course_id from Teaching_Stuff where ID ='" + Forms.UserID.ID + "'", sqlcon);
+            this.sqlcon.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            SqlCommand cmd1 = new SqlCommand("select ID from Teaching_Stuff where Course_id ='" + dr[0].ToString() + "'", sqlcon);
             try
             {
                 
                 SqlDataAdapter sda = new SqlDataAdapter();
-                sda.SelectCommand = cmd;
+                sda.SelectCommand = cmd1;
                 DataTable dbdataset = new DataTable();
                 sda.Fill(dbdataset);
                 BindingSource bsource = new BindingSource();
@@ -80,31 +85,6 @@ namespace BranchA_MazalPlus.Teaching_Assistant
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.connetionString = "Data Source = whitesnow.database.windows.net; Initial Catalog = Mazal; Integrated Security = False; User ID = Grimm; Password = #!7Dwarfs; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False; MultipleActiveResultSets=true";
-            this.sqlcon = new SqlConnection(connetionString);
-            try
-            {
-                SqlCommand cmd = new SqlCommand("select Student_Courses.stud_Id,Student_Courses.course_id, Student_Courses.grade_a, Student_Courses.grade_b, Student_Courses.grade_c, Student_Courses.quiz1, Student_Courses.quiz2, Student_Courses.final_grade FROM Student_Courses LEFT join Teaching_Stuff on Student_Courses.course_id=Teaching_Stuff.Course_ID where ID='" + Forms.UserID.ID+"'", sqlcon);
-                SqlDataAdapter sda = new SqlDataAdapter();
-                sda.SelectCommand = cmd;
-                DataTable dbdataset = new DataTable();
-                sda.Fill(dbdataset);
-                BindingSource bsource = new BindingSource();
-
-                bsource.DataSource = dbdataset;
-                StudentsReport.DataSource = bsource;
-                sda.Update(dbdataset);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                this.Close();
-                this.sqlcon.Close();
             }
         }
 
