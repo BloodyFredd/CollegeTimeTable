1cc29b57f0efb0a53a039e4c0e7f6431c074698c
Freddy M <fredtein@gmail.com>
2017-05-04 15:03:43 +0300

    Teaching_Assistant: Report of students grades

--- BranchA_MazalPlus/BranchA_MazalPlus/Teaching_Assistant/Reports.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Teaching_Assistant/Reports.cs
@@ -89,7 +89,7 @@ namespace BranchA_MazalPlus.Teaching_Assistant
             this.sqlcon = new SqlConnection(connetionString);
             try
             {
-                SqlCommand cmd = new SqlCommand("select Student_Courses.stud_Id,Student_Courses.course_id,Lecture_Course.Course_Serial FROM Student_Courses LEFT join Lecture_Course on Student_Courses.course_id=Lecture_Course.Course_ID where Teacher='" + Forms.UserID.ID+"'", sqlcon);
+                SqlCommand cmd = new SqlCommand("select Student_Courses.stud_Id,Student_Courses.course_id, Student_Courses.grade_a, Student_Courses.grade_b, Student_Courses.grade_c, Student_Courses.quiz1, Student_Courses.quiz2, Student_Courses.final_grade FROM Student_Courses LEFT join Teaching_Stuff on Student_Courses.course_id=Teaching_Stuff.Course_ID where ID='" + Forms.UserID.ID+"'", sqlcon);
                 SqlDataAdapter sda = new SqlDataAdapter();
                 sda.SelectCommand = cmd;
                 DataTable dbdataset = new DataTable();
