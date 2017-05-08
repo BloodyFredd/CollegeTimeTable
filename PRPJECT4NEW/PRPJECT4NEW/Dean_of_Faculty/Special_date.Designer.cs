50f3bc255ac1ef830306012d745169b600c5b6e4
nofar hazan <nofarha2@ac.sce.ac.il>
2017-05-08 14:59:37 +0300

    גרסא חדשה

--- PRPJECT4NEW/PRPJECT4NEW/Dean_of_Faculty/Special_date.Designer.cs
+++ PRPJECT4NEW/PRPJECT4NEW/Dean_of_Faculty/Special_date.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace PRPJECT4NEW.Exams_Section
+﻿namespace PRPJECT4NEW.Dean_of_Faculty
 {
-    partial class Special_Test
+    partial class Special_date
     {
         /// <summary>
         /// Required designer variable.
@@ -30,9 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.special_date_combobox = new System.Windows.Forms.ComboBox();
+            this.Status_lable = new System.Windows.Forms.Label();
             this.studentspecialExamBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.Combo_Course_name = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.studentspecialExamBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -42,47 +42,46 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 97);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 82);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(902, 324);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Size = new System.Drawing.Size(789, 324);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // special_date_combobox
+            // 
+            this.special_date_combobox.FormattingEnabled = true;
+            this.special_date_combobox.Location = new System.Drawing.Point(228, 25);
+            this.special_date_combobox.Name = "special_date_combobox";
+            this.special_date_combobox.Size = new System.Drawing.Size(121, 24);
+            this.special_date_combobox.TabIndex = 1;
+            this.special_date_combobox.SelectedIndexChanged += new System.EventHandler(this.special_date_combobox_SelectedIndexChanged);
+            // 
+            // Status_lable
+            // 
+            this.Status_lable.AutoSize = true;
+            this.Status_lable.Location = new System.Drawing.Point(152, 32);
+            this.Status_lable.Name = "Status_lable";
+            this.Status_lable.Size = new System.Drawing.Size(48, 17);
+            this.Status_lable.TabIndex = 2;
+            this.Status_lable.Text = "Status";
             // 
             // studentspecialExamBindingSource
             // 
             this.studentspecialExamBindingSource.DataSource = typeof(PRPJECT4NEW.Student_special_Exam);
             // 
-            // Combo_Course_name
-            // 
-            this.Combo_Course_name.FormattingEnabled = true;
-            this.Combo_Course_name.Location = new System.Drawing.Point(192, 38);
-            this.Combo_Course_name.Name = "Combo_Course_name";
-            this.Combo_Course_name.Size = new System.Drawing.Size(121, 24);
-            this.Combo_Course_name.TabIndex = 2;
-            this.Combo_Course_name.SelectedIndexChanged += new System.EventHandler(this.Combo_Course_name_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(81, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Course";
-            // 
-            // Special_Test
+            // Special_date
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 423);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Combo_Course_name);
+            this.ClientSize = new System.Drawing.Size(792, 536);
+            this.Controls.Add(this.Status_lable);
+            this.Controls.Add(this.special_date_combobox);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Special_Test";
-            this.Text = "Special_Test";
-            this.Load += new System.EventHandler(this.Special_Test_Load);
+            this.Name = "Special_date";
+            this.Text = "Special_date";
+            this.Load += new System.EventHandler(this.Special_date_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.studentspecialExamBindingSource)).EndInit();
             this.ResumeLayout(false);
@@ -91,9 +90,10 @@
         }
 
         #endregion
+
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource studentspecialExamBindingSource;
-        private System.Windows.Forms.ComboBox Combo_Course_name;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox special_date_combobox;
+        private System.Windows.Forms.Label Status_lable;
     }
 }
