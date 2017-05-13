a3d994bc30832f282f414c0ccd558ccbfec88c83
Daniel Bromberg <Daniebr1@ac.sce.ac.il>
2017-05-13 15:35:41 +0300

    Admin - Report of students with an average above 85(updated but not complete)

--- BranchA_MazalPlus/BranchA_MazalPlus/Admin/Reports.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Admin/Reports.cs
@@ -187,16 +187,16 @@ namespace BranchA_MazalPlus.Admin
             {
                 this.connetionString = "Data Source = whitesnow.database.windows.net; Initial Catalog = Mazal; Integrated Security = False; User ID = Grimm; Password = #!7Dwarfs; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
                 this.sqlcon = new SqlConnection(connetionString);
-                this.sqlcon.Open();
-                SqlCommand cmd = new SqlCommand("select AVG(final_grade),stud_ID from Student_Courses GROUP BY stud_Id" , sqlcon);
+                this.sqlcon.Open(); 
+                SqlCommand cmd = new SqlCommand("select AVG(final_grade) AS Average,stud_ID from Student_Courses GROUP BY stud_Id HAVING AVG(final_grade) >=85", sqlcon);
                 StudentsReport.Visible = true;
-                SqlDataReader dr = cmd.ExecuteReader();
-                if (dr.Read())
-                {
-                    capacity = dr[0].ToString();
-                }
-                dr.Close();
-                MessageBox.Show(capacity);
+                //SqlDataReader dr = cmd.ExecuteReader();
+                //if (dr.Read())
+                //{
+                //    capacity = dr[0].ToString();
+                //}
+                //dr.Close();
+                //MessageBox.Show(capacity);
 
 
                 SqlDataAdapter sda = new SqlDataAdapter();
