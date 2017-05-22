4b5f0fb38ba5ef9da91733d43ce8857311f64665
Eli Moshkovich <tbhjfo14@gmail.com>
2017-05-22 16:04:18 +0300

    NONE

--- BranchA_MazalPlus/BranchA_MazalPlus/Classes/Admin.cs
+++ BranchA_MazalPlus/BranchA_MazalPlus/Classes/Admin.cs
@@ -1,22 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using BranchA_MazalPlus;
 using BranchA_MazalPlus.Forms;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Data;
+using BranchA_MazalPlus.Forms;
+using System.Windows.Forms;
+using BranchA_MazalPlus.Classes;
 
 namespace BranchA_MazalPlus.Classes
 {
     class Admin : PersonStuff
     {
 
-        
-        public Admin(string id) : base(id)
-        {
 
-        }
-    }
-}
+       // public Admin(string id) : base(id)
+      //  {
+            // try try try 
+       //     public void grade56() {
+         //   try
+         //   {
+         //       General.ConnectToSql();
+         //   }
+         //   catch (Exception ex)
+         //   {
+         //       MessageBox.Show(ex.Message);
+         //   }
+         //   string value = null;
+          //  try
+           // {
+              //  PersonStuff B = new PersonStuff("123456789");
+                //SqlCommand cmd1 = new SqlCommand("SELECT course_serial FROM Student_Courses where stud_Id='" + Stud_ID.Text + "'AND course_id='" + Course.Text + "'", sqlcon);
+             //   this.sqlcon.Open();
+               // SqlDataReader dr = cmd1.ExecuteReader();
+               // if (dr.Read())
+              //  {
+                 //   value = dr[0].ToString();
+              //  }
+             //   dr.Close();
+              //  this.sqlcon.Close();
+
+             //   if (serial.Text == value) MessageBox.Show("It is the same course, try again!");
+              //  else
+              //  {
+                  //  SqlCommand cmd = new SqlCommand("UPDATE Student_Courses SET course_serial = '" + serial.Text + "' WHERE stud_Id='" + Stud_ID.Text + "'AND course_id='" + Course.Text + "'", sqlcon);
+                //    Students.Visible = true;
+                  //  SqlDataAdapter sda = new SqlDataAdapter();
+                   // sda.SelectCommand = cmd;
+                 //   DataTable dbdataset = new DataTable();
+                 //   sda.Fill(dbdataset);
+                 //   BindingSource bsource = new BindingSource();
+
+                 //   bsource.DataSource = dbdataset;
+                 //   Students.DataSource = bsource;
+                //    sda.Update(dbdataset);
+                 //   MessageBox.Show("The student was assigned!");
+             //   }
+          //  }
+          //  catch (Exception ex)
+         //   {
+               // MessageBox.Show(ex.Message);
+         //   }
+    //    }
+  //  }
+//}
+
+
+
+
+
+
+
+    
