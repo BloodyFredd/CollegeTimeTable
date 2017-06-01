92a4d33c44fc325e192eccce32d01b692bfedadf
Daniel Bromberg <Daniebr1@ac.sce.ac.il>
2017-06-01 11:39:42 +0300

    merge auditorium changes + unit test

--- UnitTestProject/AdminTest1.cs
+++ UnitTestProject/AdminTest1.cs
@@ -12,7 +12,8 @@ namespace UnitTestProject
     public class AdminTest1
     {
 
-        SignStudent f1 = new SignStudent();
+        SignStudent s1 = new SignStudent();
+        MergeAuditorium m1 = new MergeAuditorium();
         string value1 = "1001";
         string value2 = "1002";
         string serial = "1001";
@@ -21,14 +22,23 @@ namespace UnitTestProject
         [TestMethod]
         public void checkForSameSerial()
         {
-            Assert.IsTrue(f1.CheckCourse(serial, value1));
+            Assert.IsTrue(s1.CheckCourse(serial, value1));
         }
 
         //checkss if the values are different
         [TestMethod]
         public void checkForDifferentSerial()
         {
-            Assert.IsTrue(f1.CheckCourse(value2, value1));
+            Assert.IsFalse(s1.CheckCourse(value2, value1));
         }
+
+        //checkss if the values are the same
+        [TestMethod]
+        public void checkForEqual()
+        {
+            Assert.IsTrue(m1.checkChoice("", value1));
+        }
+
+        
     }
 }
